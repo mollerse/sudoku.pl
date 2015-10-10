758d92408efa80ecef0d21c5f11396fd83a4d9a9
Stian Veum Møllersen <stian.moellersen@gmail.com>
2015-10-10 16:12:55 +0200

    Add some error handling

--- server.pl
+++ server.pl
@@ -12,10 +12,14 @@ server(Port) :-
   http_server(http_dispatch, [port(Port)]).
 
 sudoku_handler(Req) :-
-  member(method(post), Req), !,
-  http_read_json_dict(Req, Data, []),
-  solve(Data, Solution),
-  reply_json(Solution).
+  (member(method(post), Req)
+    ->
+      http_read_json_dict(Req, Data, []),
+      solve(Data, Solution),
+      reply_json(Solution)
+    ;
+      throw(http_reply(server_error('Method not supported. Only POST.')))
+  ).
 
 main :-
   server(5000).
