bd2df21d12faaaece4a3228c85b252b7e91d0b96
Stian Veum Møllersen <stian.moellersen@gmail.com>
2015-11-02 18:39:44 +0100

    Add cors-support

--- server.pl
+++ server.pl
@@ -3,10 +3,13 @@
 :- use_module(library(http/thread_httpd)).
 :- use_module(library(http/http_dispatch)).
 :- use_module(library(http/http_json)).
+:- use_module(library(http/http_cors)).
 
 :- use_module(solve).
 
 :- http_handler(root(.), sudoku_handler, []).
+
+:- set_setting(http:cors, [*]).
 
 server(Port) :-
   http_server(http_dispatch, [port(Port)]).
@@ -16,6 +19,7 @@ sudoku_handler(Req) :-
     ->
       http_read_json_dict(Req, Data, []),
       solve(Data, Solution),
+      cors_enable,
       reply_json(Solution)
     ;
       throw(http_reply(server_error('Method not supported. Only POST.')))
